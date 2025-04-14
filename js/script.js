2b161ae854c87de49b3081158c68c86b446945ed
Coco29dev <10059@holbertonstudents.com>
2025-04-14 15:48:13 +0200

    implémentation html, css et script

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const BlagueTextElement = document.getElementById('blague-text');
+  const BlagueBouton = document.getElementById('blague-btn');
+
+  const API_URL = 'https://api-carambar.onrender.com/api/v1/blagues/random'
+
+  async function fetchBlagueRandom() {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        throw new Error('Une erreur est survenue');
+      }
+
+      const data = await response.json();
+    } catch (error) {
+      console.error('Erreur:', error);
+      BlagueTextElement.textContent = 'Impossible de charger une blague';
+    }
+  }
+
+  BlagueBouton.addEventListener('click', fetchBlagueRandom);
+
+});
